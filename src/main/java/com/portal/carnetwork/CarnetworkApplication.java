f30cef5273cd8b896f1977ef4a2a8f6b1af2557a
riosney <riosney_s@hotmail.com>
2026-04-04 16:47:45 -0300

    Criando um projeto CarNetwork

---
+++ src/main/java/com/portal/carnetwork/CarnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.portal.carnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetworkApplication.class, args);
+	}
+
+}
